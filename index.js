acd07c226961703ab75772a1e1fffa2b19e36707
backend dev <backend_dev44@protonmail.com>
2021-05-02 05:52:12 +0200

    remove depreciated remote object

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const { ipcRenderer } = require("electron");
 
-//function to close app for x icon on title bar
+
 function closeApp() {
   ipcRenderer.send("closeApp");
 }
 
-//function to minimize app for square icon on title bar
+
 function minimizeApp() {
   ipcRenderer.send("minimizeApp");
-  remote.BrowserWindow.getFocusedWindow().minimize();
 }
 
-//function to focus app to use it when click on app notification
-function focus() {
-  ipcRenderer.send("focus");
-}
 
 module.exports = { closeApp, minimizeApp, focus };
